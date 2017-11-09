0be79a854a3f647af058b009aa031a817a5bfac8
zhangyubaka <zhangyubaka@gmail.com>
2017-11-09 20:27:24 +0800

    fix tpyo

---
+++ main.py
@@ -0,0 +1,49 @@
+import asyncio
+from aiohttp import web
+import telepot
+import telepot.aio
+from telepot.aio.loop import OrderedWebhook
+from pprint import pprint
+from plugins.contributions import getContribution
+from config import TOKEN,PORT,URL
+
+global bot
+
+
+async def feeder(request):
+    data = await request.text()
+    webhook.feed(data)
+    return web.Response(body='OK'.encode('utf-8'))
+
+async def init(app, bot):
+    app.router.add_route('GET', '/webhook', feeder)
+    app.router.add_route('POST', '/webhook', feeder)
+
+    await bot.setWebhook(URL)
+
+
+async def handler(msg):
+	pprint(type(msg))
+	pprint(msg)
+	try:
+		if msg['text'].startswith('/contributions'):
+			await getContribution(bot,msg)
+		elif msg['text'].startswith('/start'):
+			await bot.setMessage(msg['chat']['id'],'This is Feza Bot NG.')
+		elif msg['text'].startswith('/zici'):
+			pass
+	except KeyError as e:
+		pprint(e)
+
+
+loop = asyncio.get_event_loop()
+
+app = web.Application(loop=loop)
+bot = telepot.aio.Bot(TOKEN, loop=loop)
+webhook = OrderedWebhook(bot, {'chat': handler})
+
+loop.run_until_complete(init(app, bot))
+
+loop.create_task(webhook.run_forever())
+
+web.run_app(app, port=PORT)
---
+++ plugins/contributions.py
@@ -0,0 +1,13 @@
+import requests
+import cairosvg
+
+global bot
+async def getContribution(bot,msg):
+    print('Get /Contributions')
+    # Use 'chat' instead of 'from' to avoid sending to user instead of group.
+    await bot.sendChatAction(msg['chat']['id'], 'upload_photo')
+    # Get the SVG file
+    r = requests.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions',stream=True)
+    # Convert into PNG file.
+    cairosvg.svg2png(r.raw.data,write_to='tmp.png')
+    await bot.sendPhoto(msg['chat']['id'], open('tmp.png', 'rb'),caption="GitHub Contributions for "+msg['text'].split()[-1])#reply_to_message_id=msg['message_id']
