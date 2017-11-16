64861f78cfa67067e11a615ecadae76f6a6e481b
zhangyubaka <zhangyubaka@gmail.com>
2017-11-16 22:50:04 +0800

    Nitpicking about alphabetizing methods, minor OCD thing

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from aiohttp import web
 import telepot
 import telepot.aio
 from telepot.aio.loop import OrderedWebhook
+import uvloop
 from pprint import pprint
+# Plugins
 from plugins.contributions import getContribution
 from plugins.help import botHelp
 from config import TOKEN,PORT,URL
 from plugins.trueRandom import trueInteger
 from plugins.dns import getdns
-import uvloop
 
 
-global bot
+global bot # Dirty hack stuff
 
 
-async def feeder(request):
+async def feeder(request): # Copy/Pasting code from telepot examples
     data = await request.text()
     webhook.feed(data)
     return web.Response(body='OK'.encode('utf-8'))
@@ -30,12 +31,12 @@ async def init(app, bot):
 async def handler(msg):
 	pprint(type(msg))
 	pprint(msg)
-	try:
+	try: # Handle the commands
 		if msg['text'].startswith('/contributions'):
 			await getContribution(bot,msg)
 		elif msg['text'].startswith('/start'):
 			await bot.setMessage(msg['chat']['id'],'This is Feza Bot NG.')
-		elif msg['text'].startswith('/zici'):
+		elif msg['text'].startswith('/zici'): # I might add this feature later, this is a placeholder for now.
 			pass
 		elif msg['text'].startswith('/help'):
 			await botHelp(bot,msg)
@@ -47,8 +48,8 @@ async def handler(msg):
 		pprint(e)
 
 
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-loop = asyncio.get_event_loop()
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy()) # Using libuv for better performance
+loop = asyncio.get_event_loop() # Get eventloop
 
 app = web.Application(loop=loop)
 bot = telepot.aio.Bot(TOKEN, loop=loop)
