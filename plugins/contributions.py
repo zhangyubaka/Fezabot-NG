35776fa5bc7a213078878b55f5813e41f2fd9a4e
zhangyubaka <zhangyubaka@gmail.com>
2017-11-16 14:06:07 +0800

    epic

--- plugins/contributions.py
+++ plugins/contributions.py
@@ -1,5 +1,6 @@
 import aiohttp
 import cairosvg
+import aiofiles
 
 global bot
 async def getContribution(bot,msg):
@@ -10,7 +11,8 @@ async def getContribution(bot,msg):
     async with aiohttp.ClientSession() as session:
     	async with session.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions') as resp:
     		cairosvg.svg2png(await resp.read(),write_to='tmp.png')
-    		await bot.sendPhoto(msg['chat']['id'], open('tmp.png', 'rb'),caption="GitHub Contributions for "+msg['text'].split()[-1])#reply_to_message_id=msg['message_id']
+    		async with aiofiles.open('tmp.png', 'rb') as f:
+    			await bot.sendPhoto(msg['chat']['id'],f ,caption="GitHub Contributions for "+msg['text'].split()[-1])#reply_to_message_id=msg['message_id']
     #r = requests.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions',stream=True)
     # Convert into PNG file.
     #cairosvg.svg2png(r.raw.data,write_to='tmp.png')
