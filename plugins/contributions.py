10fc0ff634be37b6661712fb42cf1b838e554242
zhangyubaka <zhangyubaka@gmail.com>
2017-11-16 13:37:46 +0800

    rats

--- plugins/contributions.py
+++ plugins/contributions.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 import cairosvg
 
 global bot
@@ -7,7 +7,10 @@ async def getContribution(bot,msg):
     # Use 'chat' instead of 'from' to avoid sending to user instead of group.
     await bot.sendChatAction(msg['chat']['id'], 'upload_photo')
     # Get the SVG file
-    r = requests.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions',stream=True)
+    async with aiohttp.ClientSession() as session:
+    	async with session.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions') as resp:
+    		cairosvg.svg2png(await resp.read(),write_to='tmp.png')
+    #r = requests.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions',stream=True)
     # Convert into PNG file.
-    cairosvg.svg2png(r.raw.data,write_to='tmp.png')
+    #cairosvg.svg2png(r.raw.data,write_to='tmp.png')
     await bot.sendPhoto(msg['chat']['id'], open('tmp.png', 'rb'),caption="GitHub Contributions for "+msg['text'].split()[-1])#reply_to_message_id=msg['message_id']
