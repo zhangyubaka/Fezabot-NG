ea4fa48f0a512ad93e4b2f503333114a5917ba4a
zhangyubaka <zhangyubaka@gmail.com>
2017-11-22 14:51:22 +0800

    Updated

---
+++ plugins/sadchildren.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import aiofiles
+import os
+import random
+
+DIR = '/root/sadchildren/'
+
+async def sad(bot,msg):
+	print('GET /sad')
+	file = random.choice(os.listdir(DIR))
+	async with aiofiles.open(file) as f:
+		await bot.sendPhoto(msg['chat']['id'],f ,caption=f.name)
+
