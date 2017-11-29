cdb4ceea48f8a18cb46b445c03b8b60383fcac9b
zhangyubaka <zhangyubaka@gmail.com>
2017-11-29 13:52:25 +0800

    Major fixup.

--- plugins/contributions.py
+++ plugins/contributions.py
@@ -1,6 +1,7 @@
 import aiohttp
+import io
 import cairosvg
-import aiofiles
+#import aiofiles
 
 global bot
 async def getContribution(bot,msg):
@@ -10,10 +11,7 @@ async def getContribution(bot,msg):
     # Get the SVG file
     async with aiohttp.ClientSession() as session:
     	async with session.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions') as resp:
-    		cairosvg.svg2png(await resp.read(),write_to='tmp.png')
-    		async with aiofiles.open('tmp.png', 'rb') as f:
-    			await bot.sendPhoto(msg['chat']['id'],f ,caption="GitHub Contributions for "+msg['text'].split()[-1])#reply_to_message_id=msg['message_id']
-    #r = requests.get('https://github.com/users/' + msg['text'].split()[-1] + '/contributions',stream=True)
-    # Convert into PNG file.
-    #cairosvg.svg2png(r.raw.data,write_to='tmp.png')
-    
+    		await bot.sendPhoto(msg['chat']['id'],io.BytesIO(cairosvg.svg2png(await resp.read())) ,caption="GitHub Contributions for "+msg['text'].split()[-1])
+    		#Convert to PNG file
+    		#async with aiofiles.open('tmp.png', 'rb') as f:
+    			
